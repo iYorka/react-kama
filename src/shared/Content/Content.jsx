9390f164133244b406d1679398add241798fee40
iYorka <yorkaz@bk.ru>
2022-02-02 14:24:36 +0300

    Day 10

--- src/shared/Content/Content.jsx
+++ src/shared/Content/Content.jsx
@@ -2,44 +2,9 @@ import React, {useEffect} from 'react';
 import styles from "./content.module.css"
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import PostsContainer from "./Posts/PostsContainer";
-import {useParams} from "react-router";
-import axios from "axios";
-import emptyAvatar from '../../assets/images/empty.png'
 
-const Content = ({setProfileIsLoading, setProfileData, profile, isLoading}) => {
+const Content = ({profile, isLoading}) => {
 
-  const params = useParams();
-  const userID = params.userID;
-  useEffect(() => {
-    if (params.userID) {
-      setProfileIsLoading(true)
-      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userID}`).then((response) => {
-        setProfileData({
-          profile: {
-            id: userID,
-            name: response.data.fullName,
-            avatar: response.data.photos.small ? response.data.photos.small : emptyAvatar,
-            status: 'status',
-            age: 'unknown',
-            info: response.data.lookingForAJob ? 'Looking for job' : 'Not looking for job'
-          }
-        })
-        setProfileIsLoading(false)
-      })
-
-    } else {
-      setProfileData({
-        profile: {
-          id: 0,
-          name: 'Me',
-          avatar: 'https://99px.ru/sstorage/56/2013/02/mid_88793_9191.jpg',
-          status: 'Bla bla bla',
-          age: 'My real age',
-          info: 'Looking for a good job'
-        }
-      })
-    }
-  }, [params.userID])
   return (
     <div className={styles.appContent}>
         <ProfileInfo profile={profile} isLoading={isLoading}/>
--- src/shared/Content/ContentContainer.jsx
+++ src/shared/Content/ContentContainer.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {connect} from "react-redux";
 import Content from "./Content";
 import {addPost, changeText, setProfileData, setProfileIsLoading} from "../redux/profileReducer";
+import {useParams} from "react-router";
+import axios from "axios";
+import emptyAvatar from "../../assets/images/empty.png";
+
+const ContentContainer = ({setProfileIsLoading, setProfileData, profile, isLoading }) => {
+  const params = useParams();
+  const userID = params.userID;
+  useEffect(() => {
+    if (params.userID) {
+      setProfileIsLoading(true)
+      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userID}`).then((response) => {
+        setProfileData({
+          profile: {
+            id: userID,
+            name: response.data.fullName,
+            avatar: response.data.photos.small ? response.data.photos.small : emptyAvatar,
+            status: 'status',
+            age: 'unknown',
+            info: response.data.lookingForAJob ? 'Looking for job' : 'Not looking for job'
+          }
+        })
+        setProfileIsLoading(false)
+      })
+
+    } else {
+      setProfileData({
+        profile: {
+          id: 0,
+          name: 'Me',
+          avatar: 'https://99px.ru/sstorage/56/2013/02/mid_88793_9191.jpg',
+          status: 'Bla bla bla',
+          age: 'My real age',
+          info: 'Looking for a good job'
+        }
+      })
+    }
+  }, [params.userID])
+  return (
+    <Content profile={profile} isLoading={isLoading}/>
+  );
+}
 
 const mapStateToProps = (state) => {
   // debugger
@@ -15,4 +56,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, {setProfileIsLoading, setProfileData, addPost, changeText})(Content);
+export default connect(mapStateToProps, {setProfileIsLoading, setProfileData, addPost, changeText})(ContentContainer);
